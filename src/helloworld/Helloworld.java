9d1b3521d0412930b72110fd0242c0c70d9b235d
HP <HP@DESKTOP-AKQIPQ6>
2019-12-11 14:00:54 +0800

    test

--- src/helloworld/Helloworld.java
+++ src/helloworld/Helloworld.java
@@ -3,7 +3,7 @@ package helloworld;
 public class Helloworld {
 
 	public static void main(String[] args) {
-		 System.out.println("Hello World 这是");
+		 System.out.println("Hello World 这是haha");
 	}
 
 }
